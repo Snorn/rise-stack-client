d8eadca07216d98af2c3d11ce9f92d7aa76e4a31
Snorn <snornn@gmail.com>
2020-07-09 11:01:07 +1000

    Setting up the React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-southeast-2",
+      BUCKET: "rise-app-uploads"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-2",
+      URL: "https://ddsxx3nkc7.execute-api.ap-southeast-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "ap-southeast-2",
+      USER_POOL_ID: "ap-southeast-2_yC0d9DGG7",
+      APP_CLIENT_ID: "15u0oisinp8kv8lbonl4stbl8k",
+      IDENTITY_POOL_ID: "ap-southeast-2:4dcad25d-6e65-44ed-be46-e9c62fb9e818"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Start with Why?</h1>
+        <p>The Inifinte Game</p>
+      </div>
+    </div>
+  );
+}
